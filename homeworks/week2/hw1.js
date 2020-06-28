9b537db01d68e7dd0550e06fd99919a6bc0cb419
Panoopan <c2200446@gmail.com>
2020-06-28 23:33:44 +0800

    week2

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,8 @@
 function printStars(n) {
-  
+    var ans = ''
+    for(var i=1; i<=n; i++){
+        console.log('*')
+    }
 }
 
 printStars(5);
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,18 @@
 function join(arr, concatStr) {
-  
+    if(arr.length === 1) return arr[0]
+    var ans = arr[0]
+    for(var i=1; i<arr.length; i++){
+        ans += (concatStr + arr[i])
+    }
+    return ans 
 }
 
 function repeat(str, times) {
-  
+    var ans = ''
+    for(var i=1; i<=times; i++){
+        ans += str
+    }
+    return ans
 }
 
 console.log(join(['a'], '!'));
